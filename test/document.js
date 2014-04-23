9574c60daf893a2afea68aacc26d05a8125a9ca9
ajlopez <webmaster@ajlopez.com>
2014-04-23 19:28:24 -0300

    find and count, naive implementation first test

--- test/document.js
+++ test/document.js
@@ -9,3 +9,13 @@ exports['Create document and to string'] = function (test) {
     test.ok(typeof doc == 'object');
     test.equal(doc.toString(), text);
 }
+
+exports['Find tags'] = function (test) {
+    var text = '<h1><div>Hello</div><div>World</div></h1>';
+    var doc = ss.document(text);
+    
+    var elements = doc.find();
+    
+    test.ok(elements);
+    test.equal(elements.count(), 3);
+}
