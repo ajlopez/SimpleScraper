7c6135e7dac8f32275dcd1464d51176d4c88e44b
ajlopez <webmaster@ajlopez.com>
2014-04-24 19:07:30 -0300

    Document new tests, WIP

--- test/document.js
+++ test/document.js
@@ -19,3 +19,42 @@ exports['Find tags'] = function (test) {
     test.ok(elements);
     test.equal(elements.count(), 3);
 }
+
+exports['Find first tag'] = function (test) {
+    var text = '<h1><div>Hello</div><div>World</div></h1>';
+    var doc = ss.document(text);
+    
+    var element = doc.find().first();
+    
+    test.ok(element);
+    test.equal(elements.tag(), "h1");
+}
+
+exports['Find each tag'] = function (test) {
+    var text = '<h1><div>Hello</div><div>World</div></h1>';
+    var doc = ss.document(text);
+    
+    var iterator = doc.find().iterator();
+    
+    test.ok(iterator);
+    
+    var element = iterator.next();
+    
+    test.ok(element);
+    test.equal(elements.tag(), "h1");
+    
+    var element = iterator.next();
+    
+    test.ok(element);
+    test.equal(elements.tag(), "div");
+    
+    var element = iterator.next();
+    
+    test.ok(element);
+    test.equal(elements.tag(), "div");
+    
+    var element = iterator.next();
+    
+    test.equal(element, null);
+}
+
