61ec90a98c5fa44ac36a555401dee72679323364
ajlopez <webmaster@ajlopez.com>
2014-05-03 07:26:03 -0300

    Find by class name first test

--- test/find.js
+++ test/find.js
@@ -1,7 +1,7 @@
 
 var ss = require('..');
 
-var text = '<h1><div>Hello</div><div>World</div></h1>';
+var text = '<h1><div class="message">Hello</div><div class="message">World</div></h1>';
 var doc = ss.document(text);
 
 exports['Find elements'] = function (test) {
@@ -62,7 +62,7 @@ exports['Get second element to string'] = function (test) {
     elements.next();
     var element = elements.next();
     
-    test.equal(element.toString(), "<div>Hello</div>");
+    test.equal(element.toString(), '<div class="message">Hello</div>');
 }
 
 exports['Get div elements'] = function (test) {
@@ -77,6 +77,24 @@ exports['Get div elements'] = function (test) {
     test.ok(element);
     test.equal(element.tag(), "div");
     test.equal(element.text(), "World");
+    
+    test.equal(elements.next(), null);
+}
+
+exports['Get elements with class message'] = function (test) {
+    var elements = doc.find(".message");
+    
+    var element = elements.next();
+    test.ok(element);
+    test.equal(element.tag(), "div");
+    test.equal(element.text(), "Hello");
+    test.equal(element.attributes().class, "message");
+    
+    var element = elements.next();
+    test.ok(element);
+    test.equal(element.tag(), "div");
+    test.equal(element.text(), "World");
+    test.equal(element.attributes().class, "message");
     
     test.equal(elements.next(), null);
 }
