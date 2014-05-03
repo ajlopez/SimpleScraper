afa895b9154bc6a971844ff4ebf2bdc3451cc70f
ajlopez <webmaster@ajlopez.com>
2014-05-03 07:12:43 -0300

    Element get attributes, first implementation and test

--- test/element.js
+++ test/element.js
@@ -5,7 +5,7 @@ var text;
 var elem;
     
 exports['Create document and to string'] = function (test) {
-    text = '<div>Hello</div>';
+    text = '<div class="message">Hello</div>';
     elem = ss.element(text);
     
     test.ok(elem);
@@ -16,5 +16,13 @@ exports['Create document and to string'] = function (test) {
 
 exports['Get text'] = function (test) {
     test.equal(elem.text(), "Hello");
+}
+
+exports['Get attributes'] = function (test) {
+    var result = elem.attributes();
+    
+    test.ok(result);
+    test.equal(typeof result, 'object');
+    test.equal(result.class, 'message');
 }
 
