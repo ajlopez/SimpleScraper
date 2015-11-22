b97053c95e13d3a22bb0d00be31a048c8059fdd6
ajlopez <webmaster@ajlopez.com>
2015-11-22 13:29:08 -0300

    Get element length

--- test/element.js
+++ test/element.js
@@ -14,6 +14,13 @@ exports['Create document and to string'] = function (test) {
     test.equal(elem.tag(), "div");
 }
 
+exports['Get element length'] = function (test) {
+    text = '<div class="message">Hello</div>';
+    elem = ss.element(text);
+    
+    test.equal(elem.length(), text.length);
+}
+
 exports['Get text'] = function (test) {
     test.equal(elem.text(), "Hello");
 }
